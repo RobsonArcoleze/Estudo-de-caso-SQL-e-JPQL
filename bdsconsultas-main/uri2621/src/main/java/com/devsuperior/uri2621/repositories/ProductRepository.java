703f7e6c9b9d72765f3e84fbeae8c9c56d6ad09c
RobsonDev <robson.arcoleze29@gmail.com>
2023-02-06 11:21:00 -0300

    SQL RAIZ + JPQL

--- bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,26 @@
 package com.devsuperior.uri2621.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2621.DTO.ProductMinDTO;
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	@Query(nativeQuery = true, value = "SELECT products.name "
+			+ "FROM products "
+			+ "INNER JOIN providers ON products.id_providers = providers.id "
+			+ "WHERE products.amount BETWEEN :min AND :max "
+			+ "AND providers.name LIKE CONCAT(:begginName, '%')")
+	List<ProductMinProjection> search1(Integer min, Integer max, String begginName);
+	
+	@Query("SELECT new com.devsuperior.uri2621.DTO.ProductMinDTO(obj.name) "
+			+ "FROM Product obj "
+			+ "WHERE obj.amount BETWEEN :min AND :max "
+			+ "AND obj.provider.name LIKE CONCAT(:begginName, '%')")
+	List<ProductMinDTO> search2(Integer min, Integer max, String begginName);
 }
